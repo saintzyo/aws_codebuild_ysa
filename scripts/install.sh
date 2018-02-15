d7d876230210397cba3f7403dc8603ffd91086c2
Ivan Morgun <saintzyo@gmail.com>
2018-02-15 13:34:07 +0200

    fixup! Add appspec and scripts

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
+cat <<EOF > /etc/yum.repos.d/nginx.repo
+[nginx]
+name=nginx repo
+baseurl=http://nginx.org/packages/centos/7/$basearch/
+gpgcheck=0
+enabled=1
+EOF
+
 yum -y install nginx > /var/log/installnginx.out 2>&1
