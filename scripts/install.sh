20c1f210fd4db50583d6b0b39529b92a69209927
Ivan Morgun <saintzyo@gmail.com>
2018-02-15 13:41:30 +0200

    fixup! Add appspec and scripts

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,7 +2,7 @@
 cat <<EOF > /etc/yum.repos.d/nginx.repo
 [nginx]
 name=nginx repo
-baseurl=http://nginx.org/packages/centos/7/$basearch/
+baseurl=http://nginx.org/packages/centos/7/\$basearch/
 gpgcheck=0
 enabled=1
 EOF
