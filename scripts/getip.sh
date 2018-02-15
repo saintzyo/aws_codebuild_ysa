ef8c13b01708eac7a3b7f969f19dc93fbd5ee186
Ivan Morgun <saintzyo@gmail.com>
2018-02-15 15:20:37 +0200

    fixup! Add appspec and scripts

--- scripts/getip.sh
+++ scripts/getip.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}' >> /usr/share/nginx/html/index.html
+/sbin/ifconfig eth0 | grep 'inet ' | cut -d: -f2 | awk '{ print $2}' >> /usr/share/nginx/html/index.html
